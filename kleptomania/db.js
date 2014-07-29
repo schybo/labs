907f095d20e157f935333f77ce0bd24c7f0c1e6e
Brent Scheibelhut <brent.scheibelhut@gmail.com>
2014-07-28 20:40:37 -0400

    First round cleanup of code

--- kleptomania/db.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://kleptomania:brent0wns@ds033569.mongolab.com:33569/kleptomania');
-
-module.exports = mongoose.connection;
--- kleptomania/public/assets/javascripts/core.js
+++
@@ -1,2 +0,0 @@
-angular.module('scotchHighscore', ['highscoreController', 'highscoreService']);
-//Maybe add back sublime code intel to sublime
