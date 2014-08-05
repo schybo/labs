5509c7744993796815d41d08f736e9ef37ff1635
Brent Scheibelhut <brent.scheibelhut@gmail.com>
2014-08-04 20:23:33 -0400

    Added qtips to blur

---
+++ public/javascripts/blur/qtips.js
@@ -0,0 +1,15 @@
+$('#easy').qtip({
+	content: {
+		text: "Just the basic <strong>blur</strong> applied to the image. You'll only get a <strong>1x</strong> score multiplier for this difficulty."
+	}
+});
+$('#medium').qtip({
+	content: {
+		text: "Both <strong>blur</strong> and <strong>hue</strong> change are applied to the image. You'll get a <strong>2x</strong> score multiplier."
+	}
+});
+$('#hard').qtip({
+	content: {
+	text: "Both <strong>blur</strong> and <strong>grayscale</strong> are applied to the image. You'll get a <strong>3x</strong> score multiplier."
+	}
+});
